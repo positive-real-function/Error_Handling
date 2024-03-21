d3d5652cf5e433d101e489f77cbd7839d870954e
Elie <jzy_dt@qq.com>
2024-03-21 21:18:19 +0800

    Modify Main.java in which a subclass of the MyChildException class is created and throw an exception to the newly created exception class in the try block and capture it.

--- CatchingExceptionHierarchy/Main.java
+++ CatchingExceptionHierarchy/Main.java
@@ -4,13 +4,20 @@ class MyParentException extends Exception {
 }
 class MyChildException extends MyParentException {
 }
+
+// 创建 MyChildException 的子类 MyGrandChildException
+class MyGrandChildException extends MyChildException {
+}
+
 public class Main {
     public static void main(String[] args) {
         try {
-            throw new MyChildException();
+            throw new MyGrandChildException(); // throwing an instance of MyGrandChildException
+        }  catch (MyGrandChildException g) { // catching MyGrandChildException
+            System.err.println("Caught MyGrandChildException");
         } catch (MyChildException s) {
             System.err.println("Caught MyChildException");
-        } catch (MyParentException a) {
+        }catch (MyParentException a) {
             System.err.println("Caught MyParentException");
         }
     }
